277e261f60be95b51c518a7a231f2b2ceaf81a22
Kleber de Andrade <kleberandrade98@gmail.com>
2022-11-23 09:50:38 -0300

    Cadastro de usuário

--- cypress/support/cad_usuario.js
+++ cypress/support/cad_usuario.js
@@ -5,6 +5,15 @@ class User{
         var cadastro_usuario = user.cadastro_usuario();
 
         cy.get('input[id="user_name"]').type(cadastro_usuario.nome)
+        cy.get('input[id="user_lastname"]').type(cadastro_usuario.sobrenome)
+        cy.get('input[id="user_email"]').type(cadastro_usuario.email)
+        cy.get('input[id="user_address"]').type(cadastro_usuario.endereco)
+        cy.get('input[id="user_university"]').type(cadastro_usuario.universidade)
+        cy.get('input[id="user_profile"]').type(cadastro_usuario.profissao)
+        cy.get('input[id="user_gender"]').type(cadastro_usuario.genero)
+        cy.get('input[id="user_age"]').type(cadastro_usuario.idade)
+        cy.get('.actions > input').click()
+        cy.get('p[id="notice"]').should('have.text', 'Usuário Criado com sucesso')
     }
 }
 
